b5396198bf6d104b5704a95358abd8dcd50fd21f
Pooja <pooja.niit@gmail.com>
2024-08-22 17:10:44 +0100

    Commented code and unused import are removed

--- app/src/main/java/com/example/harrypotterapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/harrypotterapp/ui/navigation/AppNavigation.kt
@@ -8,49 +8,23 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.harrypotterapp.model.entity.HarryPotterCharacter
 import com.example.harrypotterapp.ui.components.Loader
 import com.example.harrypotterapp.ui.view.CharacterDetailScreen
 import com.example.harrypotterapp.ui.view.HarryPotterCharactersListScreen
 import com.example.harrypotterapp.ui.viewModel.HarryPotterViewModel
 import com.example.utilities.ResultState
 
-/*@Composable
-fun AppNavigationGraph(){
-
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = Routes.CHARACTER_LIST){
-        composable(Routes.CHARACTER_LIST){
-            HarryPotterCharactersListScreen()
-        }
-
-        composable("${Routes.CHARACTER_DETAIL_SCREEN}/{name}") { backStackEntry ->
-            val name = backStackEntry.arguments?.getString("name")
-            // Retrieve character by name and display
-            CharacterDetailScreen(character = viewModel.getCharacterByName(name))
-        }
-        *//*composable(Routes.CHARACTER_DETAIL_SCREEN){
-            CharacterDetailScreen(character = )
-        }*//*
-    }
-
-}*/
-
 @Composable
 fun AppNavHost(navController: NavHostController) {
     val harryPotterViewModel: HarryPotterViewModel = hiltViewModel()
     NavHost(navController=navController, startDestination = "characterList") {
-        composable("characterList") { HarryPotterCharactersListScreen(navController = navController, harryPotterViewModel) }
+        composable(Routes.CHARACTER_LIST) { HarryPotterCharactersListScreen(navController = navController, harryPotterViewModel) }
         composable(
-            route = "characterDetail/{name}",
+            route = "${Routes.CHARACTER_DETAIL_SCREEN}/{name}",
             arguments = listOf(navArgument("name") { type = NavType.StringType })) {
             backStackEntry ->
             val name = backStackEntry.arguments?.getString("name")
-
-            // Observe the StateFlow
             val resultState by harryPotterViewModel.harryPotterCharacters.collectAsState()
 
             when (resultState) {
@@ -58,7 +32,6 @@ fun AppNavHost(navController: NavHostController) {
                     Loader()
                 }
                 is ResultState.Success -> {
-                    // Safely get the list of characters
                     val characters = (resultState as ResultState.Success).data
                     val character = characters.find { it.name == name }
                     character?.let {
